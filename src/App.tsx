c13295ee2c708ad6b2e93766921c469f80e94632
JoaoPedroCamargo <j_souza_camargo@live.com>
2021-01-17 10:23:50 -0300

    agora ta sem erro

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Router from './routes/Router';
 
+import BackgroundImg from './assets/BackgroundAll.png';
+
 const App: React.FC = () => (
-  <>
+  <div style={{ backgroundImage: `url(${BackgroundImg})`, width: '100%' }}>
     <BrowserRouter>
       <Router />
     </BrowserRouter>
     <GlobalStyle />
-  </>
+  </div>
 );
 
 export default App;
--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import {} from './styles';
-
 import NavBar from '../../components/NavBar/Navbar';
 import BottomNav from '../../components/BottomNav/BottomNav';
 
